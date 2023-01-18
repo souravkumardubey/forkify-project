2691ad6008567e6972e493c56862d7ca54542338
souravkumardubey <skdlp2000@gmail.com>
2023-01-18 18:44:31 +0530

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -107,6 +107,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -115,6 +119,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 
 init();
